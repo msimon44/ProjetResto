569d05deaa0a1061018c84f0b461b79c09df1694
mgrandbarbe <mgrandbarbe@la-joliverie.com>
2023-03-07 21:16:01 +0100

    avancement de l'it 1 + 2

--- app/src/main/java/com/example/projetresto/controleur/NousContacterActivity.java
+++ app/src/main/java/com/example/projetresto/controleur/NousContacterActivity.java
@@ -1,4 +1,13 @@
 package com.example.projetresto.controleur;
 
-public class NousContacterActivity {
+import android.widget.Button;
+import android.widget.EditText;
+import com.example.projetresto.R;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class NousContacterActivity extends AppCompatActivity {
+
+
 }
